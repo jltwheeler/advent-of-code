7185b1013d5d5b74f4bc714303a81ce315c45264
josh-wheeler-arup <jltwheeler@gmail.com>
2020-12-15 22:10:14 +0000

    Refactor of solution day 15

--- day_15/day_15.py
+++ day_15/day_15.py
@@ -1,46 +1,44 @@
 def main(inputs: list, num: int) -> int:
     numbers = [int(num) for num in inputs[0].split(",")]
 
-    results = []
     spoken = {}
+    last_num = 0
     for i in range(num):
         if i < len(numbers):
-            results.append(numbers[i])
             spoken[numbers[i]] = [{"turn": i, "count": 1}]
+            last_num = numbers[i]
             continue
 
-        last_num = results[-1]
         if len(spoken[last_num]) == 1:
-            results.append(0)
             if 0 not in spoken:
                 spoken[0] = [{"turn": i, "count": 1}]
             else:
-                spoken[0].append(
-                    {"turn": i, "count": spoken[0][-1]["count"] + 1}
-                )
+                spoken[0] = [
+                    spoken[0][-1],
+                    {"turn": i, "count": spoken[0][-1]["count"] + 1},
+                ]
+            last_num = 0
         else:
             result = (
                 spoken[last_num][-1]["turn"] - spoken[last_num][-2]["turn"]
             )
-            results.append(result)
             if result not in spoken:
                 spoken[result] = [{"turn": i, "count": 1}]
             else:
-                spoken[result].append(
-                    {"turn": i, "count": spoken[result][-1]["count"] + 1}
-                )
+                spoken[result] = [
+                    spoken[result][-1],
+                    {"turn": i, "count": spoken[result][-1]["count"] + 1},
+                ]
 
-    return results[-1]
+            last_num = result
 
-
-def part_two(inputs: list) -> int:
-    pass
+    return last_num
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         inputs = f.readlines()
 
-    print(main(inputs, 2020))
+    # print(main(inputs, 2020))
     print(main(inputs, 30000000))
 
